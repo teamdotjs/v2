71b1f2997ea7a7c2b333eb258210148a6ea9dccd
John Renner <john@jrenner.net>
2016-09-15 17:51:07 -0400

    Implement base project for client

---
+++ client/src/components/hello/hello.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./hello.scss";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export class Hello extends React.Component<HelloProps, {}> {
+    render() {
+        return <h1>Goodbye from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
