5507e39636b39f838c544606a3d1a768db5083f1
Michael Timbrook <timbrook480@gmail.com>
2016-09-29 13:15:01 -0400

    Disbable disabling static

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Vocab
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
 
+    config.api_only = true
+
     # Serve the client/dist directory
     config.public_file_server.enabled = true
     paths['public'] = File.join 'client', 'dist'
