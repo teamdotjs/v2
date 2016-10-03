9376d33175027136200b9b2e7c6fd058c4bc1636
Matthew Witte <mcw9612@rit.edu>
2016-10-03 10:56:45 -0400

    Endpoint for login and a test endpoint for the token generated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  protected
+
+  def authenticate_request!
+    if user_id_in_token?
+      @current_user = User.find(auth_token[:user_id])
+    else
+      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+    end
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated'] }, status: :unauthroized
+  end
+
+  private
+
+  def http_token
+    @http_token ||=
+      if request.headers['Authorization'].present?
+        request.headers['Authorization'].split(' ').last
+      end
+  end
+
+  def auth_token
+    @auth_token ||= JsonWebToken.decode(http_token)
+  end
+
+  def user_id_in_token?
+    http_token && auth_token && auth_token[:user_id].to_i
+  end
 end
