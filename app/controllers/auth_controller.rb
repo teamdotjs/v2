975a571aed70c7e24eb46b3620e4e2f76ef67712
Michael Timbrook <timbrook480@gmail.com>
2016-10-04 17:01:16 -0400

    Fixed ruby lint errors

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -9,10 +9,7 @@ class AuthController < ApplicationController
         value: payload(user),
         expires: 1.day.from_now
       }
-      render json: { 
-        'logged_in': true,
-        email: user.email
-      }
+      render json: { 'logged_in': true, email: user.email }
     else
       render json: { errors: ['Invalid Username/Password'] }, status: :unauthorized
     end
