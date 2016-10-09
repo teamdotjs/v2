f531dd027a2ebee64f4cf4641e4ae030ce20f93f
Matthew Witte <mcw9612@rit.edu>
2016-10-09 14:02:14 -0400

    Remove json web tokens and clean up responses (#63)

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
     scope '/auth' do
-      match '/login' => 'auth#authenticate_user', via: [:post]
-      match '/logout' => 'auth#invalidate_user', via: [:post]
+      match '/login' => 'auth#login', via: [:post]
+      match '/logout' => 'auth#logout', via: [:post]
       match '/signed_in' => 'auth#signed_in', via: [:get]
     end
 
