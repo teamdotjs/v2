c3a259a5e36acc649bdc5cc01101f47938f65e71
Matthew Witte <mcw9612@rit.edu>
2016-10-10 23:30:34 -0400

    Word and lesson endpoints stubbed

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,24 @@ Rails.application.routes.draw do
       match '/create' => 'users#create', via: [:post]
       match '/email_taken' => 'users#email_taken', via: [:get]
     end
+
+    scope '/lesson' do
+      match '/all' => 'lessons#all', via: [:get]
+      match '/create' => 'lessons#create', via: [:post]
+      match '/:lesson_id/add_word/:word_id' => 'lessons#add_word', via: [:get]
+      match '/:lesson_id/remove_word/:word_id' => 'lessons#remove_word', via: [:get]
+      match '/:id' => 'lessons#show', via: [:get]
+      match '/:id' => 'lessons#update', via: [:patch]
+      match '/:id' => 'lessons#delete', via: [:delete]
+    end
+
+    scope '/wordinfo' do
+      match '/all' => 'wordinfo#all', via: [:get]
+      match '/create' => 'wordinfo#create', via: [:post]
+      match '/:id' => 'wordinfo#show', via: [:get]
+      match '/:id' => 'wordinfo#update', via: [:patch]
+      match '/:id' => 'wordinfo#delete', via: [:delete]
+    end
   end
   #  Send static files
   match '/*path' => 'static#base', via: [:get]
