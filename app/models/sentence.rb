67f0079f257f5acc97e3bc704fb9abd26fe92bc9
Matthew Witte <mcw9612@rit.edu>
2016-11-11 10:44:39 -0500

    Validate sentence contains word or a form

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -2,4 +2,15 @@ class Sentence < ApplicationRecord
   belongs_to :wordinfo
   validates :context_sentence, presence: true
   validates_uniqueness_of :context_sentence, scope: :wordinfo, case_sensitive: false
+  validate :sentence_contains_word_or_form
+
+  private
+
+  def sentence_contains_word_or_form
+    words = []
+    words = wordinfo.forms.map(&:word) if wordinfo
+    words << wordinfo.word if wordinfo
+    return if words.any? { |word| context_sentence.include? word }
+    errors.add(:context_sentence, 'doesn\'t contain the word or a form')
+  end
 end
