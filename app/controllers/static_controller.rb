0213d320d1a74e3cb2526dd89e5eac445babb119
Michael Timbrook <timbrook480@gmail.com>
2016-11-29 09:35:11 -0500

    Linter fixes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,13 +1,11 @@
 class StaticController < ApplicationController
-
   def lesson
     file = File.read("#{Rails.root}/client/dist/index.html")
     init_data = Lesson.where(id: params[:id]).to_json
     pre_loaded_data = file.gsub(/<!--RAILS_RENDERED_INIT_DATA-->/,
-      "<script type=\"text/javascript\">" \
-      "     var LESSON_INIT_DATA = #{init_data};" \
-      "</script>" \
-    )
+                                '<script type="text/javascript">' \
+                                "     var LESSON_INIT_DATA = #{init_data};" \
+                                '</script>')
     send_data pre_loaded_data,
               type: 'text/html; charset=utf-8',
               disposition: 'inline'
@@ -18,5 +16,4 @@ class StaticController < ApplicationController
               type: 'text/html; charset=utf-8',
               disposition: 'inline'
   end
-
 end
