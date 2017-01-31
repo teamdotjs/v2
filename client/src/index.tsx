8a5a1dc57035b64cb971546ceed0ddeaf5f0db31
John Renner <john@jrenner.net>
2017-01-31 16:40:24 -0500

    Update dependencies. TSC 2.1, React 15.4.2:

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,12 +12,6 @@ import { MuiThemeProvider } from 'material-ui/styles';
 import { loginCheck } from './actions/user';
 import HomeContainer from './containers/HomeContainer';
 
-import * as injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
 const history = syncHistoryWithStore(browserHistory, store);
 
 store.dispatch(loginCheck());
