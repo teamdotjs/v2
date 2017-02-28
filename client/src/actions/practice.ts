d36ce34bd88c00220e86e57e560929bf0970503b
Michael Timbrook <timbrook480@gmail.com>
2017-02-28 13:06:02 -0500

    Update practice.ts

--- client/src/actions/practice.ts
+++ client/src/actions/practice.ts
@@ -31,7 +31,6 @@ export function generatePractice(id: number, type: SectionType) {
             dispatch({ type: 'practice_save_local', practice, id });
         })
         .catch((err: Error) => {
-            console.log(err);
             dispatch({
                 type: 'error_pin',
                 pin: PRACTICE_ERROR,
