a3b277c2b81afb1bf86d94cf61651085517ef46c
Matthew Witte <mcw9612@rit.edu>
2017-03-20 08:38:16 -0400

    Grade model

---
+++ app/models/grade.rb
@@ -0,0 +1,25 @@
+class Grade < ApplicationRecord
+  belongs_to :user
+  belongs_to :question
+  validates :value, presence: true
+
+  def as_json(options = {})
+    grade =
+      if options.empty? then super({
+        include: [
+          { user: {
+            except: [:password_digest, :created_at, :updated_at]
+          } },
+          { question: { only: [:id] } }
+        ],
+        except: [:user_id, :question_id]
+      })
+      else super(options)
+      end
+    grade['question']['prompt'] = question.prompts.first.text if grade['question']
+    grade['is_correct'] = question.options.find(&:is_correct).value.casecmp(value).zero?
+    birthday = grade&.[]('user')&.[]('birthday')
+    grade['user']['birthday'] = birthday.to_s if birthday
+    grade
+  end
+end
