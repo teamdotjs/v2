168217eebea9890b0b2e6498d67a268f45c52937
Matthew Witte <mcw9612@rit.edu>
2017-03-23 17:12:41 -0400

    Grade model and endpoints (#221)
    * Grade model
    
    * Add grades to seed
    
    * Grade endpoints

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@ class Question < ApplicationRecord
   belongs_to :practice
   has_many :prompts, inverse_of: :question, dependent: :destroy
   has_many :options, inverse_of: :question, dependent: :destroy
+  has_many :grades, dependent: :destroy
   accepts_nested_attributes_for :prompts
   accepts_nested_attributes_for :options
   enum type: [:mc, :fitb]
