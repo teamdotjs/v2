fb519432c2fdb9e46d19f5756f78d5f98b8eba89
John Sellers <jrs9025@rit.edu>
2017-04-06 17:37:54 -0400

    fixes in regex and formatting

--- client/src/components/AppBarRight.tsx
+++ client/src/components/AppBarRight.tsx
@@ -17,7 +17,7 @@ export interface PopoverState {
     anchorEl?: any;
 }
 
-export class AppBarRight extends React.Component<AppBarRightProps, PopoverState > {
+export class AppBarRight extends React.Component<AppBarRightProps, PopoverState> {
     constructor(props: AppBarRightProps) {
         super(props);
         this.state = {
@@ -26,52 +26,52 @@ export class AppBarRight extends React.Component<AppBarRightProps, PopoverState 
     }
 
     handleTouchTap = (event: any) => {
-    event.preventDefault();
+        event.preventDefault();
 
-    this.setState({
-      open: true,
-      anchorEl: event.currentTarget,
-    });
-  };
+        this.setState({
+            open: true,
+            anchorEl: event.currentTarget,
+        });
+    };
 
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
+    handleRequestClose = () => {
+        this.setState({
+            open: false,
+        });
+    };
 
- handleLogoutClose = () => {
-     this.setState({
-         open: false,
-     });
-     this.props.onLogoutClick ? this.props.onLogoutClick() : '';
- };
+    handleLogoutClose = () => {
+        this.setState({
+            open: false,
+        });
+        this.props.onLogoutClick ? this.props.onLogoutClick() : '';
+    };
 
-  render() {
-      let loggedIn = (
-        <div style={{marginTop: '6px'}}>
-            <FlatButton
-            style={{color: 'white'}}
-            label={this.props.userName || ''}
-            onTouchTap={this.handleTouchTap}
-            />
-            <Popover
-            open={this.state.open}
-            anchorEl={this.state.anchorEl}
-            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-            targetOrigin={{horizontal: 'left', vertical: 'top'}}
-            onRequestClose={this.handleRequestClose}
-            >
-            <Menu>
-                <MenuItem primaryText='User Profile' />
-                <MenuItem primaryText='Sign out'
-                onTouchTap={this.handleLogoutClose} />
-            </Menu>
-            </Popover>
-        </div>
+    render() {
+        let loggedIn = (
+            <div style={{ marginTop: '6px' }}>
+                <FlatButton
+                    style={{ color: 'white' }}
+                    label={this.props.userName || ''}
+                    onTouchTap={this.handleTouchTap}
+                />
+                <Popover
+                    open={this.state.open}
+                    anchorEl={this.state.anchorEl}
+                    anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+                    targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+                    onRequestClose={this.handleRequestClose}
+                >
+                    <Menu>
+                        <MenuItem primaryText='User Profile' />
+                        <MenuItem primaryText='Sign out'
+                            onTouchTap={this.handleLogoutClose} />
+                    </Menu>
+                </Popover>
+            </div>
         );
 
-        return this.props.userName ? loggedIn : <div/>;
+        return this.props.userName ? loggedIn : <div />;
     };
 };
 export default AppBarRight;
