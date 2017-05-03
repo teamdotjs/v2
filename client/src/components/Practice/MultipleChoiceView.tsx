d46776f87ab73a08c3ad5ab95e532229e0df6036
Michael Timbrook <timbrook480@gmail.com>
2017-05-02 23:12:04 -0400

    Radio buttons work too

--- client/src/components/Practice/MultipleChoiceView.tsx
+++ client/src/components/Practice/MultipleChoiceView.tsx
@@ -17,13 +17,30 @@ const styles = {
   },
 };
 
-export class MultipleChoiceView extends React.Component<QuestionViewProps, {}> {
+interface MultiState {
+    value?: any;
+}
+
+export class MultipleChoiceView extends React.Component<QuestionViewProps, MultiState> {
+
+    constructor(props: QuestionViewProps) {
+        super(props);
+        this.state = { value: props.value };
+    }
+
+    onChange(value: any) {
+        if (this.props.onChange) this.props.onChange(value, this.props.question.id);
+        this.setState({value});
+    }
 
     render(): JSX.Element {
         const question: Question = this.props.question;
         return <div>
             <h3>{question.prompts[0]} </h3>
-            <RadioButtonGroup name='response' valueSelected={this.props.value}>
+            <RadioButtonGroup 
+                name='response' 
+                valueSelected={this.state.value}
+                onChange={ (_: any, newOpt: any) => this.onChange(newOpt) }>
                 {question.options.map((opt) => {
                     return <RadioButton
                             style={styles.radioButton}
